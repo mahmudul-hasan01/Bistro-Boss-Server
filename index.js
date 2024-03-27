2d244d902741bbd296dfe097dcb3fc6088b203fc
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-03-28 00:10:07 +0600

    get menu data

--- index.js
+++ index.js
@@ -27,9 +27,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
+        // await client.connect(); 
+
+        const menu = client.db("BistroDB").collection("Menu");
+
+        //menu
+
+        app.get('/menu', async (req, res) => {
+            const result = await menu.find().toArray()
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
